8c6dd2dd5cb9e7902ab9f31c17136251c3fddadb
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-28 18:18:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
-def nyc_pigeon_organizer(data)
-  # write your code here!
+
+  def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+    data.collect do |key, value| #key are categories :color and value are cat-data
+      value.collect do |k,v| # k are sub-cats "purple","male" and v are array of name data 
+        v.collect do |names|
+            pigeon_list[names] ||= {}
+            pigeon_list[names][key] ||= []
+            pigeon_list[names][key] << k.to_s
+        end
+      end
+   end
+   pigeon_list
 end
