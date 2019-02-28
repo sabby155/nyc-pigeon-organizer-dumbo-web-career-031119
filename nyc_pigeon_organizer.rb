ed8b83eb101b192258717310359651ef090bcbc1
Sabrina Chowdhury <nebula155@gmail.com>
2019-02-28 18:17:09 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  
+  def nyc_pigeon_organizer(data)
+  pigeon_list = {}
+    data.collect do |key, value| #key are categories :color and value are cat-data
+      value.collect do |k,v| # k are sub-cats "purple","male" and v are array of name data 
+        v.collect do |names|
+            pigeon_list[names] ||= {}
+            pigeon_list[names][key] ||= []
+            pigeon_list[names][key] << k
+        end
+      end
+   end
+   pigeon_list
+end
 end
